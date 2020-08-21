251a8fe1f77da10b1ff851ec357c4b584de61a09
DESKTOP-325D3AA\612110081 <rujrawee_rua@cmu.ac.th>
2020-08-21 15:20:07 +0700

    21/08/2020

---
+++ Assets/ArissaControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArissaControl : MonoBehaviour
+{
+    Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            _animator.SetTrigger("Flair");
+        }
+         if(Input.GetKeyDown(KeyCode.S))
+        {
+            _animator.SetTrigger("Hurricane");
+        }
+         if(Input.GetKeyDown(KeyCode.D))
+        {
+            _animator.SetBool("Martelo",true);
+        }
+        else if(Input.GetKeyUp(KeyCode.D))
+        {
+            _animator.SetBool("Martelo",false);
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            _animator.SetBool("Salsa Dance",true);
+        }
+        else if(Input.GetKeyUp(KeyCode.W))
+        {
+            _animator.SetBool("Salsa Dance",false);
+        }
+    }
+}
